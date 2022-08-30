8f57dfb035ffc9a59b6d82c261a1618c4d2df2e9
Joey <xjoey96@gmail.com>
2022-08-30 03:22:34 -0500

    Update links for deployment

--- src/components/CurrentlyPlaying.js
+++ src/components/CurrentlyPlaying.js
@@ -142,7 +142,7 @@ function CurrentlyPlaying(props) {
             setRplSrc(
               "https://www.youtube.com/embed/" +
                 response.result.items[0].id.videoId +
-                "?autoPlay=1&origin=http://localhost:3000"
+                "?autoPlay=1&origin=https://Joey-Red.github.io/music-with-friends-front-end"
             );
           },
           function (err) {
@@ -167,7 +167,7 @@ function CurrentlyPlaying(props) {
           setRplSrc(
             "https://www.youtube.com/embed/" +
               response.result.items[0].id.videoId +
-              "?autoPlay=1&origin=http://localhost:3000"
+              "?autoPlay=1&origin=https://Joey-Red.github.io/music-with-friends-front-end"
           );
         },
         function (err) {
